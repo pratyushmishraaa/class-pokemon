9bf31e4e2189cad0309d801b31cf636648933ea1
pratyushmishraaa <pratyushmishra287@gmail.com>
2023-01-22 18:05:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+// import pokemon from './pokemon.json'
+import PokemonInfo from './components/PokemonInfo'
+import PokemonRow from './components/PokemonRow'
+import PokemonSearch from './components/PokemonSearch'
+import PokemonTable from './components/PokemonTable'
+
+
+
+
+
 
 function App() {
+
+  console.log('app rendered')
+  const [filter, setFilter] = useState('')
+  const [pokemon, setPokemon] = useState([])
+
+
+  // fetch("/my-app/pokemon.json")
+
+  useEffect(() => {
+    console.log('useeffect run')
+    //this will run when the component mounts and only once
+    fetch('pokemon.json')
+      .then(response => response.json())
+      .then(data => setPokemon(data))
+      .catch(function (error) { console.log(error) })
+
+  }, [])
+
+
+
+  //two rules of hooks
+  //it should be on top level management
+  //it should be inside react function
+  const [selectedPokemon, setSelectedPokemon] = useState(null)
+  // console.log('selectedPokemon', selectedPokemon)
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      margin: 'auto',
+      width: 800,
+      paddingTop: '1em'
+    }} >
+      <h1 className='title'>Pokemon Search</h1>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: "80% 20%",
+          gridColumnGap: '1rem'
+        }}
+      >
+        <div>
+
+          <PokemonSearch filter={filter} setFilter={setFilter} />
+          <PokemonTable pokemon={pokemon} filter={filter} setSelectedPokemon={setSelectedPokemon} />
+          
+        </div>
+        
+
+
+        {selectedPokemon && <PokemonInfo {...selectedPokemon} />}
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
